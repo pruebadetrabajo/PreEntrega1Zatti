e6fbbe2312d6c4596017fb47767e5aa3ba8992ae
Claudio <claudiozatti@gmail.com>
2023-03-23 21:48:40 -0300

    Se actualiza estilos

--- my-app/src/components/NavBar/NavBar.jsx
+++ my-app/src/components/NavBar/NavBar.jsx
@@ -3,18 +3,24 @@ import { CartWidget } from "../CartWidget/CartWidget";
 export function NavBar() {
     return  (
         <>
-            <nav id="nav">
-                <ul>   
-                    <li><img src="./src/img/logo.png" id="carrito" alt="Carrito de compras"/></li>
-                    <li><a href="#">Deportes</a></li>
-                    <li><a href="#">Hogar</a></li>
-                    <li><a href="#">Electro</a></li>
-                    <li><a href="#">Viajes</a></li>
-                    <li><a href="#">Ofertas</a></li>
-                    <li><CartWidget></CartWidget></li>
-                </ul>
-            </nav>
-            
+            <div id="contenedorNavBar">
+                <div id="contenedorLogo">
+                <img src="./src/img/logo.png" id="logo" alt="Carrito de compras"/>
+                </div>
+                <div id="contenedorNav">
+                    <nav id="nav">
+                        <ul>   
+                            <li><a href="#">Deportes</a></li>
+                            <li><a href="#">Hogar</a></li>
+                            <li><a href="#">Electro</a></li>
+                            <li><a href="#">Viajes</a></li>
+                            <li><a href="#">Ofertas</a></li>
+                            <li><CartWidget></CartWidget></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+                        
         </>
     )
 }
